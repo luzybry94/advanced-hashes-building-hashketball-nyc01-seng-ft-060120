3116aba3ab6fd812de6a6b136738ecdac7d54bdb
Chaim Bryski <luzybry94@gmail.com>
2020-05-20 15:08:36 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,8 +4,20 @@ def game_hash
     home: {
       team_name: "Brooklyn Nets"
       colors: [Black, White]
-      players: [{player_name: }]
-          }
+      players: [
+        {player_name: 
+         number:
+         shoe:
+         points:
+         rebounds:
+         assists:
+         steals:
+         blocks:
+         slam_dunks:
+        }
+        
+              ]
+    }
     away: {}
   }
   
