22a56f0e4eeb856ff9b8c89ce41b386591bf1bad
Chaim Bryski <luzybry94@gmail.com>
2020-05-20 15:40:22 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,10 +2,11 @@
 def game_hash
   {
     home: {
-      team_name: "Brooklyn Nets"
-      colors: ["Black", "White"]
+      team_name: "Brooklyn Nets",
+      colors: ["Black", "White"],
       players: [
-        {player_name: "Alan Anderson",
+        {
+          player_name: "Alan Anderson",
          number: 0,
          shoe: 16,
          points: 22,
@@ -16,7 +17,8 @@ def game_hash
          slam_dunks: 1
         },
         
-         {player_name: "Reggie Evans",
+         {
+         player_name: "Reggie Evans",
          number: 30,
          shoe: 14,
          points: 12,
@@ -27,7 +29,8 @@ def game_hash
          slam_dunks: 7
         }, 
         
-        {player_name: "Brook Lopez",
+        {
+         player_name: "Brook Lopez",
          number: 11,
          shoe: 17,
          points: 17,
@@ -38,7 +41,8 @@ def game_hash
          slam_dunks: 15
         },
         
-         {player_name: "Mason Plumlee",
+         {
+         player_name: "Mason Plumlee",
          number: 1,
          shoe: 19,
          points: 26,
@@ -49,7 +53,8 @@ def game_hash
          slam_dunks: 5
         }, 
         
-         {player_name: "Jason Terry",
+         {
+         player_name: "Jason Terry",
          number: 31,
          shoe: 15,
          points: 19,
@@ -62,10 +67,11 @@ def game_hash
         ]
     }
     away: {
-      team_name: "Charlotte Hornets"
-      colors: ["Turquoise", "Purple"]
+      team_name: "Charlotte Hornets",
+      colors: ["Turquoise", "Purple"],
       players: [
-        {player_name: "Jeff Adrien",
+        {
+         player_name: "Jeff Adrien",
          number: 4,
          shoe: 18,
          points: 10,
@@ -76,7 +82,8 @@ def game_hash
          slam_dunks: 2
         },
         
-         {player_name: "Bismack Biyombo"
+         {
+         player_name: "Bismack Biyombo"
          number: 0,
          shoe: 16,
          points: 12,
@@ -87,7 +94,8 @@ def game_hash
          slam_dunks: 10
         }, 
         
-        {player_name: "DeSagna Diop",
+        {
+         player_name: "DeSagna Diop",
          number: 2,
          shoe: 14,
          points: 24,
@@ -98,7 +106,8 @@ def game_hash
          slam_dunks: 5
         },
         
-         {player_name: "Ben Gordon", 
+         {
+         player_name: "Ben Gordon", 
          number: 8,
          shoe: 15,
          points: 33,
@@ -109,7 +118,8 @@ def game_hash
          slam_dunks: 0
         }, 
         
-         {player_name: "Kemba Walker", 
+         {
+         player_name: "Kemba Walker", 
          number: 33,
          shoe: 15,
          points: 6,
