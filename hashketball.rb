50831e2060a303a60b31d11142d13f729deb45da
Chaim Bryski <luzybry94@gmail.com>
2020-05-20 15:13:22 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -14,11 +14,113 @@ def game_hash
          steals:
          blocks:
          slam_dunks:
-        }
+        },
         
-              ]
+         {player_name: 
+         number:
+         shoe:
+         points:
+         rebounds:
+         assists:
+         steals:
+         blocks:
+         slam_dunks:
+        }, 
+        
+        {player_name: 
+         number:
+         shoe:
+         points:
+         rebounds:
+         assists:
+         steals:
+         blocks:
+         slam_dunks:
+        },
+        
+         {player_name: 
+         number:
+         shoe:
+         points:
+         rebounds:
+         assists:
+         steals:
+         blocks:
+         slam_dunks:
+        }, 
+        
+         {player_name: 
+         number:
+         shoe:
+         points:
+         rebounds:
+         assists:
+         steals:
+         blocks:
+         slam_dunks:
+        }
+        ]
     }
-    away: {}
+    away: {
+      team_name: "Brooklyn Nets"
+      colors: [Black, White]
+      players: [
+        {player_name: 
+         number:
+         shoe:
+         points:
+         rebounds:
+         assists:
+         steals:
+         blocks:
+         slam_dunks:
+        },
+        
+         {player_name: 
+         number:
+         shoe:
+         points:
+         rebounds:
+         assists:
+         steals:
+         blocks:
+         slam_dunks:
+        }, 
+        
+        {player_name: 
+         number:
+         shoe:
+         points:
+         rebounds:
+         assists:
+         steals:
+         blocks:
+         slam_dunks:
+        },
+        
+         {player_name: 
+         number:
+         shoe:
+         points:
+         rebounds:
+         assists:
+         steals:
+         blocks:
+         slam_dunks:
+        }, 
+        
+         {player_name: 
+         number:
+         shoe:
+         points:
+         rebounds:
+         assists:
+         steals:
+         blocks:
+         slam_dunks:
+        }
+        ]
+    }
   }
   
 end
